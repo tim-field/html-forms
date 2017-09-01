06bbd882f519c6bcf24624b2db8e2decd2c975f3
Danny <dannyvankooten@gmail.com>
2017-09-01 11:16:39 +0200

    Initial commit

---
+++ html-forms.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: HTML Forms
+Plugin URI: https://htmlformsplus.com/#utm_source=wp-plugin&utm_medium=html-forms&utm_campaign=plugins-page
+Description: Not just another forms plugin. Simple and flexible.
+Version: 1.0
+Author: ibericode
+Author URI: https://ibericode.com/
+License: GPL v3
+Text Domain: html-forms
+
+HTML Forms Plus
+Copyright (C) 2017, Danny van Kooten, danny@ibericode.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
