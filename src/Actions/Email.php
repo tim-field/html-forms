a847c1e30b30130590dd43b1bbc74e10f6b9e1e2
Danny <dannyvankooten@gmail.com>
2017-12-08 13:29:06 +0100

    add content-type and additional header setting to email action. closes #8

--- src/Actions/Email.php
+++ src/Actions/Email.php
@@ -14,9 +14,27 @@ class Email extends Action {
        $this->label = __( 'Send Email', 'html-forms' );
    }
 
+   /**
+   * @return array
+   */
+   private function get_default_settings() {
+       $defaults = array(
+          'from' => get_option( 'admin_email' ),
+          'to' => get_option( 'admin_email' ),
+          'subject' => '',
+          'message' => '',
+          'headers' => '', 
+          'content_type' => 'text/html',
+       );
+       return $defaults;
+   }
+
+   /**
+   * @param array $settings
+   * @param string|int $index
+   */ 
    public function page_settings( $settings, $index ) {
-       $defaults = array_fill_keys( array( 'from', 'to', 'subject', 'message' ), '' );
-       $settings = array_merge( $defaults, $settings );
+       $settings = array_merge( $this->get_default_settings(), $settings );
        ?>
        <span class="hf-action-summary"><?php printf( 'From %s. To %s.', $settings['from'], $settings['to']  ); ?></span>
        <input type="hidden" name="form[settings][actions][<?php echo $index; ?>][type]" value="<?php echo $this->type; ?>" />
@@ -39,11 +57,29 @@ class Email extends Action {
                    <input name="form[settings][actions][<?php echo $index; ?>][subject]" value="<?php echo esc_attr( $settings['subject'] ); ?>" type="text" class="regular-text" placeholder="<?php echo esc_attr( __( 'Your email subject', 'html-forms' ) ); ?>" />
                </td>
            </tr>
+            
            <tr>
                <th><label><?php echo __( 'Message', 'html-forms' ); ?> <span class="hf-required">*</span></label></th>
                <td>
                    <textarea name="form[settings][actions][<?php echo $index; ?>][message]" rows="8" class="widefat" placeholder="<?php echo esc_attr( __( 'Your email message', 'html-forms' ) ); ?>" required><?php echo esc_textarea( $settings['message'] ); ?></textarea>
-                    <p class="help"><?php _e( 'You can use the following variables: ', 'html-forms' ); ?><span class="hf-field-names"></span></p>
+                   <p class="help"><?php _e( 'You can use the following variables (in all fields): ', 'html-forms' ); ?><span class="hf-field-names"></span></p>                 
+               </td>
+           </tr>
+
+           <tr>
+               <th><label><?php echo __( 'Content Type', 'html-forms' ); ?></label></th>
+               <td>
+                   <select name="form[settings][actions][<?php echo $index; ?>][content_type]" required>
+                      <option <?php selected( $settings['content_type'], 'text/html' ); ?>>text/plain</option>
+                      <option <?php selected( $settings['content_type'], 'text/html' ); ?>>text/html</option>
+                   </select>
+               </td>
+           </tr>
+        
+           <tr>
+               <th><label><?php echo __( 'Additional headers', 'html-forms' ); ?></label></th>
+               <td>
+                   <textarea name="form[settings][actions][<?php echo $index; ?>][headers]" rows="4" class="widefat" placeholder="<?php echo esc_attr( 'Reply-To: [NAME] <[EMAIL]>' ); ?>"><?php echo esc_textarea( $settings['headers'] ); ?></textarea>
                </td>
            </tr>
        </table>
@@ -62,10 +98,18 @@ class Email extends Action {
            return;
        }
 
+       $settings = array_merge( $this->get_default_settings(), $settings );
+
        $to = hf_template( $settings['to'], $submission->data );
        $subject = ! empty( $settings['subject'] ) ? hf_template( $settings['subject'], $submission->data ) : '';
-       $message = nl2br( hf_template( $settings['message'], $submission->data ) );
-       $headers = array( 'Content-Type: text/html' );
+       $message = hf_template( $settings['message'], $submission->data );
+       $headers = explode( PHP_EOL, $settings['headers'] );
+       $html_email = $settings['content_type'] === 'text/html';
+
+       if( $html_email ) {
+          $headers[] = 'Content-Type: text/html';
+          $message = nl2br( $message );
+       }
 
        if( ! empty( $settings['from'] ) ) {
            $from = hf_template($settings['from'], $submission->data);
